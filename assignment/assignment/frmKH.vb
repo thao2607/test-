547708ff196fd0f97e0499c18e7d8e59c5d62841
Nguyễn Thảo <Nguyn Tho>
2016-03-05 08:18:47 +0700

    thao

---
+++ assignment/assignment/frmKH.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class frmKH
+    Dim db As New DataTable
+    Dim chuoiketnoi As String = "workstation id=Ps03551QLBH.mssql.somee.com;packet size=4096;user id=longlhps03551_SQLLogin_1;pwd=81ueofcgj6;data source=Ps03551QLBH.mssql.somee.com;persist security info=False;initial catalog=Ps03551QLBH"
+    Dim connect As SqlConnection = New SqlConnection(chuoiketnoi)
+    Private Sub btnXem_Click(sender As Object, e As EventArgs) Handles btnXem.Click
+        Dim connect As SqlConnection = New SqlConnection(chuoiketnoi)
+        connect.Open()
+        Dim xem As SqlDataAdapter = New SqlDataAdapter("select MAKH as 'Mã KH' ,TENKH as 'Tên Khách Hàng', DIACHI as 'Địa chỉ', SDT as 'SĐT', EMAIL from KHACHANG where MAKH='" & txtMAKH.Text & "'", connect)
+        Try
+            If txtMAKH.Text = "" Then
+                MessageBox.Show("Bạn cần nhập MAKH", "Nhập thiếu", MessageBoxButtons.OKCancel, MessageBoxIcon.Error)
+          
+            Else
+                db.Clear()
+                dgvXemkh.DataSource = Nothing
+                xem.Fill(db)
+                If db.Rows.Count > 0 Then
+                    dgvXemkh.DataSource = db.DefaultView
+                    txtMAKH.Text = Nothing
+                Else
+                    MessageBox.Show("Không tìm thấy")
+                    txtMAKH.Text = Nothing
+                End If
+            End If
+            connect.Close()
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    Private Sub btnXemall_Click(sender As Object, e As EventArgs) Handles btnXemall.Click
+        Dim hienthi As New Class1
+        dgvXemkh.DataSource = hienthi.Loadkhachang.Tables(0)
+    End Sub
+
+    Private Sub btnDong_Click(sender As Object, e As EventArgs) Handles btnDong.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmKH_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ assignment/assignment/frmLoaisanpham.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmLoaisanpham
+
+    Private Sub frmLoaisanpham_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
